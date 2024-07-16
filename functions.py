ed594e1d8ff151957ba0b3f5e4fd99c61303c5c3
Alyssa Malinoff <alyssamalinoff@gmail.com>
2024-07-16 14:41:57 -0700

    Created a class for both functions

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import re
+#import re 
 import os
 import wordninja
 import shutil
--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import os
 import shutil
 from functions import FileSorter  # Update import
 
+'''
+TODO
+-Fix the file move process to make sure the files are renamed beforehand
+    -or dont? it might be easier to move the files first because we know all the source files will be lowercase. 
+    Once they're sorted we can run FileScrubber? This would also make sorting easier as the dict values could just be lowercase 
+'''
+
 def main():
-    source_directory = input("Please enter the source directory path: ") #U:\Coding\PersonalProjects\LibraryTestSource
+    source_directory = input("Please enter the source directory path: ") #E:\LibraryTestSource
+    sorter = FileSorter(source_directory, destination_directory, category_dict)
+    sorter.sort_and_rename_files()
+
 
     if not os.path.isdir(source_directory):
         print("The provided source directory does not exist.")
         return
 
-    destination_directory = input("Please enter the destination directory path: ") #U:\Coding\PersonalProjects\LibraryTestDest
+    destination_directory = input("Please enter the destination directory path: ") #E:\LibraryTestDest
     category_dict = {
         'AWS': ['AWS', 'Amazon'],
         'Google': ['Google'],
